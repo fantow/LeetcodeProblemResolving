8588f550eff3d867662ab19ff035a11b684c1cc3
fantow <1727611626@qq.com>
2020-08-07 09:48:08 +0800

    Problem 14_2

--- src/main/java/JZOffer/P_14_1.java
+++ src/main/java/JZOffer/P_14_1.java
@@ -8,13 +8,14 @@ package JZOffer;
  */
 public class P_14_1 {
     public static void main(String[] args) {
-        int n = 2;
+        int n = 10;
         int i = cuttingRope(n);
 
         System.out.println(i);
     }
 
-    public static int cuttingRope(int n) {
+    // 贪心算法，需要证明
+    public static int cuttingRope1(int n) {
         if(n == 2){
             return 1;
         }
@@ -37,4 +38,33 @@ public class P_14_1 {
 
         return -1;
     }
+
+    // 动态规划
+    public static int cuttingRope(int n){
+        if(n == 2){
+            return 1;
+        }else if(n == 3){
+            return 2;
+        }
+
+        int[] arr = new int[n + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 4;i <= n;i++){
+           for(int j = 1;j <= i/2;j++){
+               int product = arr[j] * arr[i - j];
+               if(max < product){
+                   max = product;
+               }
+           }
+           arr[i] = max;
+        }
+        return arr[n];
+    }
+
 }
---
+++ src/main/java/JZOffer/P_14_2.java
@@ -0,0 +1,66 @@
+package JZOffer;
+
+/**
+ * @Description:
+ * @Author chenyang270
+ * @CreateDate
+ * @Version: 1.0
+ */
+public class P_14_2 {
+
+    public static long[] arr;
+
+    public static void main(String[] args) {
+        int n = 211;
+        long result1 = cuttingRope1(n);
+
+        System.out.println(result1);
+    }
+
+    // 方法1.贪心算法
+    public static int cuttingRope1(int n) {
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+
+        int count = n / 3;
+        int mod = n % 3;
+
+        if(mod == 0){
+            return (int)getMod2(3,count,1000000007);
+        }else if(mod == 1){
+            return (int)((getMod2(3,count - 1,1000000007) * 4) % 1000000007);
+        }else if(mod == 2){
+            return (int)((getMod2(3,count - 1,1000000007) * 6) % 1000000007);
+        }
+        return -1;
+    }
+
+    // 大数求余算法1:循环求余
+    public static long getMod1(int x,int a,int p){
+        long result = 1;
+
+        for(int i = 1;i <= a;i++){
+            result = (result * x) % p;
+        }
+
+        return result;
+    }
+
+    // 大数求余算法2:快速幂求余
+    public static long getMod2(long x,int a,int p){
+        long result = 1;
+        while (a > 0){
+            if(a % 2 == 1){
+                result = (result * x) % p;
+            }
+            x = x * x % p;
+            a /= 2;
+        }
+        return result;
+    }
+
+}
