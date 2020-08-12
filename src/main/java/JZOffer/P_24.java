ccb83595f82413a1fa51f01656519d824196705d
fantow <1727611626@qq.com>
2020-08-12 08:38:35 +0800

    Problem 24

---
+++ src/main/java/JZOffer/P_24.java
@@ -0,0 +1,62 @@
+package JZOffer;
+
+/**
+ * @Description:
+ * @Author chenyang270
+ * @CreateDate
+ * @Version: 1.0
+ */
+public class P_24 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode temp = head;
+        int[] arr = {2,3,4,5};
+        for(int a : arr){
+            ListNode node = new ListNode(a);
+            temp.next = node;
+            temp = node;
+        }
+
+        head = reverseList2(head);
+        while (head != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+
+    }
+
+    // 头插法
+    public static ListNode reverseList1(ListNode head) {
+        ListNode newHead = new ListNode(-1);
+        ListNode temp = head;
+
+        while(head != null){
+            temp = head.next;
+            head.next = newHead.next;
+            newHead.next = head;
+            head = temp;
+        }
+
+        return newHead.next;
+    }
+
+    // 递归方法
+    public static ListNode reverseList2(ListNode head) {
+
+        ListNode node = reverse(head);
+        return node;
+    }
+
+    public static ListNode reverse(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode temp = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return temp;
+    }
+
+}
