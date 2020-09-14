c49063780e8a65d3dea5d598ad0fdd1ec3c1bab2
fantow <1727611626@qq.com>
2020-09-14 20:11:35 +0800

    Problem 151

--- src/main/java/Classify400/String/基础/P_151.java
+++ src/main/java/Classify400/String/基础/P_151.java
@@ -21,42 +21,11 @@ public class P_151 {
 //        String s = " ";
 
 //        ---- test5 ----
-//        String s = "  ";
+        String s = "  ";
 //
-//        String result = reverseWords(s);
-//        System.out.println(result);
-
-        String A = "123456";
-        int b = 100;
-
-        String[] results = div(A,b);
-        for(String str : results){
-            System.out.print(str + " ");
-        }
-        System.out.println();
+        String result = reverseWords(s);
+        System.out.println(result);
     }
-
-    public static String[] div(String A,int b){
-        String[] results = new String[2];
-
-        int t = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i < A.length();i++){
-            t = t * 10 + (A.charAt(i) - '0');
-            if(t / b != 0){
-                sb.append(t / b);
-            }
-            t = t % b;
-        }
-
-
-        results[0] = sb.toString();
-        results[1] = String.valueOf(t);
-
-        return results;
-    }
-
-
 
 
     public static String reverseWords(String s) {
