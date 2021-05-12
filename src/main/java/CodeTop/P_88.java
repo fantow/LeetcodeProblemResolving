9e4d69739e6a07fc799fc20e08f86e1c5a201980
fantow <1727611626@qq.com>
2021-05-12 21:01:55 +0800

    P_88

---
+++ src/main/java/CodeTop/P_88.java
@@ -0,0 +1,59 @@
+package CodeTop;
+
+import java.util.Arrays;
+
+public class P_88 {
+    public static void main(String[] args) {
+
+//        int[] nums1 = {1,2,3,0,0,0};
+//        int m = 3;
+//
+//        int[] nums2 = {2,5,6};
+//        int n = 3;
+
+        int[] nums1 = {0};
+        int m = 0;
+
+        int[] nums2 = {1};
+        int n = 1;
+
+        merge(nums1,m,nums2,n);
+
+        for(int num : nums1){
+            System.out.print(num + " ");
+        }
+    }
+
+    // 从后向前确定数组顺序
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        if(m == 0){
+//            for(int i = 0;i < n;i++){
+//                nums1[i] = nums2[i];
+//            }
+//            return;
+//        }
+
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+
+        int idx = m + n - 1;
+
+        while(ptr1 >= 0 && ptr2 >= 0){
+            if(nums1[ptr1] >= nums2[ptr2]){
+                nums1[idx--] = nums1[ptr1];
+                ptr1--;
+            }else{
+                nums1[idx--] = nums2[ptr2];
+                ptr2--;
+            }
+        }
+
+        while(ptr1 >= 0){
+            nums1[idx--] = nums1[ptr1--];
+        }
+
+        while(ptr2 >= 0){
+            nums1[idx--] = nums2[ptr2--];
+        }
+    }
+}
